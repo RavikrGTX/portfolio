70cc7ce0c0ea95a05784574030ae8584cf88e6ab
ravi <ravikumarmamidi17@gmail.com>
2026-05-16 23:37:25 +0530

    chatbot

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,10 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Projects from "./components/Projects";
+
+import ChatBot from "./components/ChatBot";
+
+
 export default function Home() {
   return (
     <>
@@ -22,7 +26,7 @@ export default function Home() {
     <Projects/>
     <Contact/>
     <Footer/>
-    
+    <ChatBot/>
     </>
   );
 }
